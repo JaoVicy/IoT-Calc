314b939b5230816a0282bcb9c446c4607e234c91
JaoVicy <filipimjoa@gmail.com>
2025-07-29 16:53:36 -0300

    chore: added GeoPoint model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,1 +1,11 @@
 from django.db import models
+
+class GeoPoint(models.Model):
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    distance = models.FloatField(null=True, blank=True)  # Distância até o ponto de referência (opcional)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"({self.latitude}, {self.longitude}) - {self.distance or 'N/A'} km"
