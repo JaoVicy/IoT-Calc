8c758e2e6a145b8180b31e8a80b4c09a0a67b4b9
Joao Alfredo Williges Cunha <joaoalfredowc@gmail.com>
2025-07-29 15:22:12 -0300

    chore: fixing things about the gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,15 @@
 const gulp = require ('gulp');
 const sass = require('gulp-sass')(require('sass'));
+
+function styles(){
+    return gulp.src('src/scss/**/*.scss'
+        .pipe(sass().on)('error', sass.logError))
+        .pipe(gulp.dest('dist/css'));   
+}
+
+function watchFiles(){
+    gulp.watch('src/scss/**/*.scss', styles);
+}
+
+exports.styles = styles;
+exports.watch = watchFiles;
