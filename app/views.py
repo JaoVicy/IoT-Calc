ebfefd54d771a99a4a40f2ee87176a3c6c3b1b99
JaoVicy <filipimjoa@gmail.com>
2025-07-30 15:37:09 -0300

    chore: added imports

--- app/views.py
+++ app/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# for def distance_calculator:
+from django.shortcuts import render
+from .form import DistanceForm
+from geopy.distance import geodesic
+
 def index(request):
     return render(request, 'app/index.html')
