176dbc55eb4bea4ecbcd948fe728ebada026a596
JaoVicy <filipimjoa@gmail.com>
2025-07-30 15:22:12 -0300

    chore: added code

--- app/form.py
+++ app/form.py
@@ -1,1 +1,21 @@
 from django import forms
+
+from django import forms
+
+class DistanceFrom(forms.Form):
+    lat1 = forms.FloatField(
+        label='Latitude 1',
+        help_text='Enter the first latitude in decimal degrees.'
+    )
+    lon2 = forms.FloatField(
+        label='Longitude 2',
+        help_text='Enter the second longitude in decimal degrees.'
+    )
+    lat2 = forms.FloatField(
+        label='Latitude 2',
+        help_text='Enter the second latitude in decimal degrees.'
+    )
+    lon2 = forms.FloatField(
+        label='Longitude 2',
+        help_text='Enter the first longitude in decimal degrees.'
+    )
