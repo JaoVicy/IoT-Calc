df18395ed03ff8cdfc00585bd52fdb77e1447c68
JaoVicy <filipimjoa@gmail.com>
2025-08-01 19:35:32 -0300

    perf: formated with black

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,5 @@ from django.urls import include, path
 from app.views import index
 
 urlpatterns = [
-    path('', index, name='index') # This will route the root URL to the index view\
-
+    path("", index, name="index")  # This will route the root URL to the index view\
 ]
