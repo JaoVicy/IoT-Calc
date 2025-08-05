1cbb2e86b7a738b57fb6221df55a030ac85d8716
JaoVicy <filipimjoa@gmail.com>
2025-08-05 18:26:13 -0300

    chore: formated with black

--- app/form.py
+++ app/form.py
@@ -5,18 +5,14 @@ from django import forms
 
 class DistanceForm(forms.Form):
     lat1 = forms.FloatField(
-        label='Latitude 1',
-        help_text='Coloque a primeira latitude em decimais.'
+        label="Latitude 1", help_text="Coloque a primeira latitude em decimais."
     )
     lon1 = forms.FloatField(
-        label='Longitude 1',
-        help_text='Coloque a primeira longitude em decimais.'
+        label="Longitude 1", help_text="Coloque a primeira longitude em decimais."
     )
     lat2 = forms.FloatField(
-        label='Latitude 2',
-        help_text='Coloque a segunda latitude em decimais.'
+        label="Latitude 2", help_text="Coloque a segunda latitude em decimais."
     )
     lon2 = forms.FloatField(
-        label='Longitude 2',
-        help_text='Coloque a segunda longitude em decimais.'
+        label="Longitude 2", help_text="Coloque a segunda longitude em decimais."
     )
--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,3 @@ from app.views import index
 urlpatterns = [
     path("", index, name="index")  # This will route the root URL to the index view\
 ]
-
