ade845eab202467a9bd129a44c5ccd179e2b56a6
JaoVicy <filipimjoa@gmail.com>
2025-08-08 13:45:18 -0300

    chore: added try to calc code

--- app/views.py
+++ app/views.py
@@ -8,21 +8,29 @@ from geopy.distance import geodesic
 
 
 def index(request):
-    result = None
-    if request.method == "POST":
-        form = DistanceForm(request.POST)
-        if form.is_valid():
-            lat1 = form.cleaned_data["lat1"]
-            lon1 = form.cleaned_data["lon1"]
-            lat2 = form.cleaned_data["lat2"]
-            lon2 = form.cleaned_data["lon2"]
-            point1 = (lat1, lon1)
-            point2 = (lat2, lon2)
-            distance = geodesic(point1, point2).kilometers
-            result = f"{distance:.2f} km"
-    else:
-        form = DistanceForm()
-    return render(request, "app/index.html", {"form": form, "result": result})
+
+    try:
+        result = None
+        if request.method == "POST":
+            form = DistanceForm(request.POST)
+            if form.is_valid():
+                lat1 = form.cleaned_data["lat1"]
+                lon1 = form.cleaned_data["lon1"]
+                lat2 = form.cleaned_data["lat2"]
+                lon2 = form.cleaned_data["lon2"]
+                point1 = (lat1, lon1)
+                point2 = (lat2, lon2)
+                distance = geodesic(point1, point2).kilometers
+                result = f"{distance:.2f} km"
+        else:
+            form = DistanceForm()
+
+    except ValueError:
+        return HttpResponse("Invalid input. Please enter valid coordinates.")   
+    except Exception as e:
+        return HttpResponse(f"An error occurred: {e}")
+    
+        return render(request, "app/index.html", {"form": form, "result": result})
 
 
 """"
